6591d4878b4e9bb6bf85c121e0e89b6a880e922c
Joseph Crail <jbcrail@gmail.com>
2016-12-23 14:39:33 -0500

    Add FSM implementations

---
+++ machines/button.py
@@ -0,0 +1,99 @@
+from enums import UniqueIntEnum
+from fsm import FSM
+
+
+class ButtonState(UniqueIntEnum):
+    IdleUp = 1
+    IdleDown = 2
+    Hover = 3
+    HoverPressed = 4
+    Pressed = 5
+    HeldOutside = 6
+    Fire = 7
+
+    @classmethod
+    def default(cls):
+        return cls.IdleUp
+
+
+class ButtonEvent(UniqueIntEnum):
+    Press = 1
+    Release = 2
+    Enter = 3
+    Leave = 4
+
+
+class ButtonFSM(FSM):
+    """
+    A FSM implementation for a standard pushbutton control.
+
+    States are enumerated values as defined by the UniqueIntEnum class. See
+    ButtonState class for full enumerated values.
+
+    Based on http://web.stanford.edu/class/archive/cs/cs103/cs103.1142/button-fsm/
+    """
+
+    def __init__(self):
+        super(ButtonFSM, self).__init__(ButtonState, ButtonEvent)
+
+        self.add_transition(ButtonState.IdleUp,
+                            ButtonEvent.Press,
+                            ButtonState.IdleDown)
+
+        self.add_transition(ButtonState.IdleUp,
+                            ButtonEvent.Enter,
+                            ButtonState.Hover)
+
+        self.add_transition(ButtonState.IdleDown,
+                            ButtonEvent.Release,
+                            ButtonState.IdleUp)
+
+        self.add_transition(ButtonState.IdleDown,
+                            ButtonEvent.Enter,
+                            ButtonState.HoverPressed)
+
+        self.add_transition(ButtonState.HoverPressed,
+                            ButtonEvent.Leave,
+                            ButtonState.IdleDown)
+
+        self.add_transition(ButtonState.HoverPressed,
+                            ButtonEvent.Release,
+                            ButtonState.Hover)
+
+        self.add_transition(ButtonState.Hover,
+                            ButtonEvent.Leave,
+                            ButtonState.IdleUp)
+
+        self.add_transition(ButtonState.Hover,
+                            ButtonEvent.Press,
+                            ButtonState.Pressed)
+
+        self.add_transition(ButtonState.Pressed,
+                            ButtonEvent.Release,
+                            ButtonState.Fire)
+
+        self.add_transition(ButtonState.Pressed,
+                            ButtonEvent.Leave,
+                            ButtonState.HeldOutside)
+
+        self.add_transition(ButtonState.HeldOutside,
+                            ButtonEvent.Enter,
+                            ButtonState.Pressed)
+
+        self.add_transition(ButtonState.HeldOutside,
+                            ButtonEvent.Release,
+                            ButtonState.IdleUp)
+
+
+class Button(object):
+    def __init__(self):
+        self.fsm = ButtonFSM()
+        self._state = self.fsm.states.default()
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        self._state = value
---
+++ machines/regex.py
@@ -0,0 +1,91 @@
+from enums import UniqueIntEnum
+from fsm import FSM
+
+
+class PatternState(UniqueIntEnum):
+    Start = 1
+    LeadingA = 2
+    B = 3
+    TrailingA = 4
+    Accept = 5
+    Reject = 6
+
+    @classmethod
+    def default(cls):
+        return cls.Start
+
+
+class PatternEvent(UniqueIntEnum):
+    A = 1
+    B = 2
+    EOF = 3
+
+
+class PatternFSM(FSM):
+    """
+    A FSM implementation for a regex pattern [A*BA*].
+
+    States are enumerated values as defined by the UniqueIntEnum class. See
+    PatternState class for full enumerated values.
+    """
+
+    def __init__(self):
+        super(PatternFSM, self).__init__(PatternState, PatternEvent)
+
+        self.add_transition(PatternState.Start,
+                            PatternEvent.A,
+                            PatternState.LeadingA)
+
+        self.add_transition(PatternState.Start,
+                            PatternEvent.B,
+                            PatternState.B)
+
+        self.add_transition(PatternState.Start,
+                            PatternEvent.EOF,
+                            PatternState.Reject)
+
+        self.add_transition(PatternState.LeadingA,
+                            PatternEvent.A,
+                            PatternState.LeadingA)
+
+        self.add_transition(PatternState.LeadingA,
+                            PatternEvent.B,
+                            PatternState.B)
+
+        self.add_transition(PatternState.B,
+                            PatternEvent.A,
+                            PatternState.TrailingA)
+
+        self.add_transition(PatternState.B,
+                            PatternEvent.B,
+                            PatternState.Reject)
+
+        self.add_transition(PatternState.B,
+                            PatternEvent.EOF,
+                            PatternState.Accept)
+
+        self.add_transition(PatternState.TrailingA,
+                            PatternEvent.A,
+                            PatternState.TrailingA)
+
+        self.add_transition(PatternState.TrailingA,
+                            PatternEvent.B,
+                            PatternState.Reject)
+
+        self.add_transition(PatternState.TrailingA,
+                            PatternEvent.EOF,
+                            PatternState.Accept)
+
+
+class Pattern(object):
+    def __init__(self):
+        self.fsm = PatternFSM()
+        self._state = self.fsm.states.default()
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        self._state = value
